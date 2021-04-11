091c5fd9568e4b7e74d044070273ae4469e557a5
Thales Gomes <thalessud@gmail.com>
2021-04-11 14:52:28 +0100

    basic setup

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
+import SearchBar from './SearchBar'
 
 class App extends React.Component {
   render() {
     return (
-      <div>App</div>
+      <div className="ui container"><SearchBar /></div>
     )
   }
 }
---
+++ src/components/SearchBar.jsx
@@ -0,0 +1,34 @@
+import React from 'react';
+
+class SearchBar extends React.Component {
+  state = { term: '' };
+
+  onInputChange = e => {
+    this.setState({ term: e.target.value });
+  };
+  
+  handleSubmit = e => {
+    e.preventDefault();
+    // callback from parent component
+  };
+
+  render() {
+    return (
+      <div className="search-bar ui segment">
+        <form className="ui form" onSubmit={this.handleSubmit}>
+          <div className="field">
+            <label htmlFor="">Video Search</label>
+            <input
+              type="text"
+              value={this.state.term}
+              onChange={this.onInputChange}
+            />
+          </div>
+        </form>
+          
+      </div>
+    )
+  }
+}
+
+export default SearchBar;
